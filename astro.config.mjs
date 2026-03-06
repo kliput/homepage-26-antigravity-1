6aad08f8ef68d988a368e35eb29c0b5d98add18c
Jakub Liput <jakub.liput@gmail.com>
2026-03-06 11:48:58 +0100

    Added sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://onedata.org',
   image: {
     responsiveStyles: true,
     layout: "constrained",
-  }
+  },
+  integrations: [sitemap()]
 });
