d8bb45e820e49a5a99bc93b27122ee00e228b918
Jakub Liput <jakub.liput@gmail.com>
2026-03-09 15:20:17 +0100

    Copied some styles from base44 project

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,18 +6,27 @@ import rehypeExternalLinks from 'rehype-external-links';
 import { remarkRewriteLinks } from "./src/plugins/remark-rewrite-links.mjs";
 import { remarkNormalizeLanguages } from "./src/plugins/remark-normalize-languages.mjs";
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://onedata.org',
+
   image: {
     responsiveStyles: true,
     layout: "constrained",
   },
+
   markdown: {
     remarkPlugins: [remarkRewriteLinks, remarkNormalizeLanguages],
     rehypePlugins: [
       [rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }]
     ],
   },
-  integrations: [sitemap()]
+
+  integrations: [sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
