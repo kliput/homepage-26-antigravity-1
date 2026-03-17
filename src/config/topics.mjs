eb18820c2da9537ca34325f269fc21be65587c82
Jakub Liput <jakub.liput@gmail.com>
2026-03-17 22:16:12 +0100

    Added topic aliases support

---
+++ src/config/topics.mjs
@@ -0,0 +1,41 @@
+export default {
+  "about-onedata": "intro",
+  "admin-rest-api": "admin-guide/onezone/configuration/rest-api",
+  "data-access-control": "user-guide/data#data-access-control",
+  "demo-mode": "admin-guide/demo-mode",
+  "file-popularity-function":
+    "admin-guide/oneprovider/configuration/file-popularity#the-popularity-function",
+  luma: "admin-guide/oneprovider/configuration/luma",
+  metadata: "user-guide/metadata",
+  "oidc-saml": "admin-guide/onezone/configuration/oidc-saml",
+  "oidc-saml-authority-delegation":
+    "admin-guide/onezone/configuration/oidc-saml#authority-delegation",
+  "oidc-saml-custom-entitlement-parsers":
+    "admin-guide/onezone/configuration/oidc-saml#custom-entitlement-parsers-advanced",
+  oneclient: "user-guide/interfaces/oneclient",
+  "oneclient-direct-io":
+    "user-guide/interfaces/oneclient#direct-io-and-proxy-io-modes",
+  "onedatify-deploy": "admin-guide/oneprovider/installation/onedatify-cli",
+  oneprovider: "admin-guide/oneprovider/installation",
+  "oneprovider-prerequisites":
+    "admin-guide/oneprovider/prerequisites#prerequisites--requirements",
+  onezone: "admin-guide/onezone/installation",
+  "onezone-dns-config": "admin-guide/onezone/configuration/dns-config",
+  qos: "admin-guide/oneprovider/configuration/rule-based-replication-qos",
+  tokens: "user-guide/tokens",
+  "tokens-access": "user-guide/tokens#access-tokens",
+  "tokens-caveats": "user-guide/tokens#token-caveats",
+  "tokens-consumer": "user-guide/tokens#consumer",
+  "tokens-data-access-caveats": "user-guide/tokens#data-access-caveats",
+  "tokens-gui-guide": "user-guide/tokens#gui-guide",
+  "tokens-identity": "user-guide/tokens#identity-tokens",
+  "tokens-invite": "user-guide/tokens#invite-tokens",
+  "tokens-named-temporary": "user-guide/tokens#named-and-temporary-tokens",
+  "tokens-rest-api": "user-guide/tokens#using-rest-api",
+  "tokens-safe-publishing": "user-guide/tokens#safely-publishing-tokens",
+  "tokens-security-privacy":
+    "user-guide/tokens#security-and-privacy-considerations",
+  "tokens-service": "user-guide/tokens#service",
+  "user-quickstart": "user-guide/quickstart",
+  "web-certificate": "admin-guide/onezone/configuration/web-certificate",
+};
