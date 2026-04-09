889dc3167d24c2970ec42ccd99b1ca2085626294
Jakub Liput <jakub.liput@gmail.com>
2026-04-09 13:01:59 +0200

    Updated domain in get-api.sh

--- utils/get-api.sh
+++ utils/get-api.sh
@@ -3,6 +3,6 @@ set -e
 
 cd "$(dirname "$0")/.."
 
-curl http://localhost:8000/api/all.zip -o /tmp/all.zip
+curl http://get.onedata.org/api/all.zip -o /tmp/all.zip
 unzip /tmp/all.zip -d ./src/content/api
 rm /tmp/all.zip
