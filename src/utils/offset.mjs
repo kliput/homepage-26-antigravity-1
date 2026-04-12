f9fb4f6357d2e520f82a3ec10cf49933503a8ab1
Jakub Liput <jakub.liput@gmail.com>
2026-04-12 21:09:36 +0200

    Independent scroll for docs/api sidebar

--- src/utils/offset.mjs
+++
@@ -1,8 +0,0 @@
-/**
- * @param {HTMLElement} element
- * @returns {number}
- */
-export function viewportOffsetTop(element) {
-  const boundingClientRect = element.getBoundingClientRect();
-  return boundingClientRect.top;
-}
