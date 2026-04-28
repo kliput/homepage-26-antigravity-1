8c65bc48823acefec6f55b956a55601420810978
Jakub Liput <jakub.liput@gmail.com>
2026-04-28 16:32:36 +0200

    Build docker using bamboos scripts

--- utils/package.sh
+++ utils/package.sh
@@ -4,5 +4,8 @@ set -e
 cd "$(dirname "$0")/.."
 
 export TAG="ID-`git rev-parse HEAD | cut -b -10`-T`date +%s`"
-echo $TAG
-docker build -t docker.onedata.org/homepage-26:${TAG} .
+./docker_build.py --name homepage \
+                  --tag $TAG \
+                  --repository docker.onedata.org \
+                  .
+                  # TODO: add user, password, publish, remove?
