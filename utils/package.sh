88fbadbe08e50d1592e6b4f3bd8355a952ee147a
Jakub Liput <jakub.liput@gmail.com>
2026-04-28 13:14:25 +0200

    Fixed package.sh

--- utils/package.sh
+++ utils/package.sh
@@ -5,4 +5,4 @@ cd "$(dirname "$0")/.."
 
 export TAG="ID-`git rev-parse HEAD | cut -b -10`-T`date +%s`"
 echo $TAG
-docker build -t docker.onedata.org/homepage-26:${TAG}
+docker build -t docker.onedata.org/homepage-26:${TAG} .
